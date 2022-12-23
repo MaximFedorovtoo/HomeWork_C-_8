9d0390318340c51103fd2b3760866a1a4f3b6995
Maxim <makfedorovtooo@gmail.com>
2022-12-23 19:00:14 +0300

    do with loop while

--- program_5/Program.cs
+++ program_5/Program.cs
@@ -18,35 +18,28 @@ void PrintArray(int [,] array)
 }
 int [,] AddNumbRound(int [,] array)
 {
-    int [ ] numbers = new int [array.GetLength(0)*array.GetLength(1)];
-    int tmp = 1;
-    for (int i = 0; i < numbers.Length; i++)
-    {
-        numbers[i] = tmp;
-        tmp++;
-    }
-    int count_1 = 0;
-    int count_2 = array.GetLength(1);
-    int count_3 = array.GetLength(0)+array.GetLength(1)+1;
-    int count_4 = array.GetLength(0)+array.GetLength(1)+array.GetLength(1)-1;
-    int count_5 = array.GetLength(0)+array.GetLength(1)+array.GetLength(1);
-    int count_6 = array.GetLength(0)+array.GetLength(1)+array.GetLength(1)+array.GetLength(1)-1;
+    
+   
+    int n = array.GetLength(0);
 
-    for (int i = 0; i < array.GetLength(0); i++)
+    int i = 0, j = 0;
+
+    int value = 1;
+
+    while (n != 0)
     {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            if(i == 0){array[i,j] = numbers[count_1];}
-            if(j == array.GetLength(1)-1 & i != 0){array[i,j] = numbers[count_2]; count_2++;}
-            if(i == array.GetLength(0)-1 & j != array.GetLength(1)-1 & i != 0){array[i,j] = numbers[count_3]; count_3--;}
-            if(j == 0 & i != array.GetLength(0)-1 & j != array.GetLength(1)-1 & i != 0){array[i,j] = numbers[count_4]; count_4--;}
-            if(i == 1 & j != 0 & i != array.GetLength(0)-1 & j != array.GetLength(1)-1 & i != 0){array[i,j] = numbers[count_5]; count_5++;}
-            if(i == 2 & i != 1 & j != 0 & i != array.GetLength(0)-1 & j != array.GetLength(1)-1 & i != 0){array[i,j] = numbers[count_6]; count_6--;}
-            count_1++;
-        }
-        
+        int k = 0;
+        do { array[i, j++] = value++; } 
+        while (++k < n - 1);
+        for (k = 0; k < n - 1; k++) array[i++, j] = value++;
+        for (k = 0; k < n - 1; k++) array[i, j--] = value++;
+        for (k = 0; k < n - 1; k++) array[i--, j] = value++;
+        ++i; ++j; 
+        n = n < 2 ? 0 : n - 2;
     }
     return array;
+    
+    
 }
 
 int [,] array = new int [4,4];
