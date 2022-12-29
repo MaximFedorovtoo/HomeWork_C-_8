46a78f8ad8b8b499bb718d0312b263c110fb4f2f
Maxim <makfedorovtooo@gmail.com>
2022-12-29 22:43:39 +0300

    fix program_5

--- program_5/Program.cs
+++ program_5/Program.cs
@@ -19,14 +19,16 @@ void PrintArray(int [,] array)
 int [,] AddArrayRound(int [,] array)
 {
     int n = array.GetLength(0);
+    int n_1 = array.GetLength(1);
     int count = 1;
     for (int i = 0, j = 0, k = 0; n != 0; i++,j++)
     {
-        for (k = 0; k < n - 1; k++) {array[i, j++] = count++;}
+        for (k = 0; k < n_1 - 1; k++) {array[i, j++] = count++;}
         for (k = 0; k < n - 1; k++) {array[i++, j] = count++;}
-        for (k = 0; k < n - 1; k++) {array[i, j--] = count++;}
+        for (k = 0; k < n_1 - 1; k++) {array[i, j--] = count++;}
         for (k = 0; k < n - 1; k++) {array[i--, j] = count++;}
         n = n < 2 ? 0 : n - 2;
+        n_1 = n_1 < 2 ? 0 : n_1 - 2;
     }
     return array;
 }
